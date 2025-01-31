ea8d99ed6a05d90d8e9cdfa6f952b6082f5d9cb3
L3vi Ackerman <l3vi3211@gmail.com>
2025-01-31 17:52:36 +0545

    addition of login and logout forms

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.Home, name="home")]
+urlpatterns = [
+    path("", views.Home, name="home"),
+    path("about", views.About, name="about"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+]
